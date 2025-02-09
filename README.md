6356dfa33a99e14527042177891e8def1bb4d8a2
theirix <theirix@gmail.com>
2025-02-09 11:31:24 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# retlsproxy
+
+[![Crates.io](https://img.shields.io/crates/v/cloudwatch_metrics_agent.svg)](https://crates.io/crates/cloudwatch_metrics_agent)
+[![Build](https://github.com/theirix/retlsproxy/actions/workflows/build.yml/badge.svg)](https://github.com/theirix/retlsproxy/actions/workflows/build.yml)
+
+An HTTP proxy to tweak TLS capabilities.
+
+## Installation
+
+    cargo install retlsproxy
+
+## Usage
+
+    retlsproxy --listen localhost:3000 --target=https://weaktls.net.example
+
+## License
+
+BSD 3-Clause
---
+++ src/main.rs
@@ -0,0 +1,187 @@
+use axum::{body::Body, extract::Request, response::IntoResponse, response::Response};
+use reqwest;
+use url::Url;
+
+use http::uri::Authority;
+use http::Uri;
+use http::{Method, StatusCode};
+use hyper::body::Incoming;
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use hyper_util::server;
+use tokio::net::TcpListener;
+use tower::Service;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Listen address
+    #[structopt(short, long)]
+    listen: String,
+
+    /// Target host
+    #[structopt(short, long)]
+    target: String,
+
+    /// User-agent
+    #[structopt(short, long)]
+    user_agent: Option<String>,
+
+    /// Trace requests
+    #[structopt(long)]
+    trace: bool
+}
+
+/// Proxy to a TLS service with reduced capabilities
+#[derive(Clone)]
+struct Proxy {
+    /// Target authority (with port and without scheme)
+    target_authority: Authority,
+    /// Target hostname (with port and scheme)
+    target_host: String,
+    /// Pre-configured client for a target service
+    client: reqwest::Client,
+}
+
+impl Proxy {
+    pub fn new(
+        target: String,
+        user_agent: Option<String>,
+        trace: bool
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        // Compose target host
+        let target_host = Self::get_target_host(&target)?;
+        let target_authority = target_host.parse()?;
+        assert!(!target_host.starts_with("http"));
+        let target_host_tls: String = "https://".to_owned() + &target_host;
+
+        // Prepopulate headers
+        let mut headers = http::HeaderMap::with_capacity(2);
+        headers.insert("Referer", target_host_tls.parse().unwrap());
+        headers.append("Origin", target_host_tls.parse().unwrap());
+        if let Some(value) = user_agent {
+            headers.append("User-Agent", value.parse().unwrap());
+        }
+        tracing::info!("Using headers: {:?}", &headers);
+
+        // Precreate a TLS client
+        // - HTTP1-only
+        // - no TLS verification
+        // - Special case for headers
+        let client = reqwest::ClientBuilder::new()
+            .http1_only()
+            .http1_title_case_headers()
+            .danger_accept_invalid_certs(true)
+            .default_headers(headers.clone())
+            .connection_verbose(trace)
+            .build()?;
+        Ok(Self {
+            client,
+            target_authority,
+            target_host,
+        })
+    }
+
+    /// Get target host without scheme
+    fn get_target_host(target: &String) -> Result<String, Box<dyn std::error::Error>> {
+        let target_uri =
+            Url::parse(&target).or_else(|err| Err(format!("Cannot parse target host: {}", err)))?;
+        let target_host = target_uri
+            .host_str()
+            .ok_or_else(|| "Cannot parse target host: {}")?;
+        Ok(target_host.into())
+    }
+
+    fn compose_target_uri(&self, source_uri: &Uri) -> Result<Uri, Box<dyn std::error::Error>> {
+        let target_uri: Uri = Uri::builder()
+            .scheme("https")
+            .authority(self.target_authority.clone())
+            .path_and_query(source_uri.to_string())
+            .build()
+            .or_else(|err| Err(format!("Cannot build target url: {}", err)))?;
+        Ok(target_uri)
+    }
+
+    async fn serve_request(&self, req: Request) -> Result<Response, Box<dyn std::error::Error>> {
+        let target_uri: Uri = self
+            .compose_target_uri(req.uri())
+            .map_err(|err| format!("Wrong target URI {} with err={}", &self.target_host, err))?;
+
+        let method = match *req.method() {
+            Method::GET => reqwest::Method::GET,
+            Method::POST => reqwest::Method::POST,
+            _ => return Err("Unsupported method".into()),
+        };
+
+        // Pass the request stream directly to the target service
+        let request_body_stream = req.into_body().into_data_stream();
+        let request_body = reqwest::Body::wrap_stream(request_body_stream);
+        let resp = self
+            .client
+            .request(method, target_uri.to_string())
+            .body(request_body)
+            .send()
+            .await;
+        let response = resp?.bytes().await?;
+        let response: Response = Response::new(response.into());
+        Ok(response)
+    }
+
+    async fn retls(&self, req: Request) -> Result<Response, hyper::Error> {
+        tracing::trace!(?req);
+
+        match self.serve_request(req).await {
+            Ok(response) => Ok(response),
+            Err(err) => {
+                // Serve 404 on any error
+                tracing::error!(err);
+                return Ok((StatusCode::BAD_REQUEST, "Error serving request").into_response());
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::builder()
+            .with_default_directive(tracing_subscriber::filter::LevelFilter::INFO.into())
+            .from_env_lossy()
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+
+    // Create a retls proxy
+    let opt = Opt::from_args();
+    let proxy = Proxy::new(opt.target, opt.user_agent, opt.trace).unwrap();
+
+    let tower_service = tower::service_fn(move |req: Request<_>| {
+        let proxy = proxy.clone();
+        let req = req.map(Body::new);
+        async move { proxy.retls(req).await }
+    });
+
+    let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
+        tower_service.clone().call(request)
+    });
+
+    tracing::info!("listening on {}", &opt.listen);
+
+    let listener = TcpListener::bind(opt.listen).await.unwrap();
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        let io = TokioIo::new(stream);
+        let hyper_service = hyper_service.clone();
+        tokio::task::spawn(async move {
+            if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
+                .serve_connection_with_upgrades(io, hyper_service)
+                .await
+            {
+                tracing::error!("Failed to serve connection: {:?}", err);
+            }
+        });
+    }
+}
